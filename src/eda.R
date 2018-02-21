3d6830fd12e1c24f09a2c74a5d5ded5d28f966d6
Arthur Lui <luiarthur@gmail.com>
2018-02-20 21:58:00 -0800

    yes

--- src/eda.R
+++ src/eda.R
@@ -4,9 +4,11 @@ library(maps)
 path = "../dat/Crimes_-_2001_to_present.csv"
 #crimes = loadDF(path, "csv")
 crimes = read.df(path, "csv", header="true", inferSchema="true")
-dim(crimes)
-head(crimes)
-
+colnames_df<-colnames(crimes)
+colnames_df<-gsub(" ","_",colnames_df)
+colnames(crimes)<-colnames_df
+#dim(crimes)
+#head(crimes)
 
 #N = 10000
 #small = take(dropna(crimes),N) # Take 10000
@@ -19,3 +21,15 @@ N = NROW(small)
 
 quilt.plot(small$Lon,small$Lat,rep(1,N), bty='n', axes=F)
 map('county', add=TRUE, col='grey30')
+
+### Small model ###
+mod = stats::glm(as.factor(Arrest) ~ as.factor(`Primary_Type`), family='binomial', data=small)
+
+### Spark Model ###
+ccrimes = dropna(crimes)
+newDF = select(ccrimes, cast(ccrimes$Arrest, "Int"), ccrimes$Primary_Type, ccrimes$District)
+
+### FIXME ??? ###
+#modSpark = spark.glm(newDF, Arrest ~ Primary_Type, regParam=0.5, family='binomial')
+modSpark <- spark.logit(newDF, Arrest ~ Primary_Type, regParam=0.5)
+s <- summary(modSpark)
